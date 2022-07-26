00cc9bb9eebe27e1f807aca961c377be1b50643c
Xander Palmer <xanderpalmer7@gmail.com>
2022-07-26 17:32:05 -0400

    Updated Login Page

--- ToDoList/core/forms.py
+++ ToDoList/core/forms.py
@@ -19,6 +19,12 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'input'
+            visible.field.widget.attrs['id'] = visible.name
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
